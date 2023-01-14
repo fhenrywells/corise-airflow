b9e1c6fa2fba71b5c869cc7dbdf7f27f98bef1ab
Henry Weller <henryweller@Henrys-Air.lan>
2023-01-14 17:52:28 -0500

    Fix startup script

--- start.sh
+++ start.sh
@@ -1,11 +1,19 @@
-# Install brew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"  
+# if using mac, install via brew
 
-echo '# Set PATH, MANPATH, etc., for Homebrew.' >> /Users/henryweller/.zprofile
-echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/henryweller/.zprofile
-eval "$(/opt/homebrew/bin/brew shellenv)"
+if [[ $1 == 'mac' ]]
+then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"  
+    echo '# Set PATH, MANPATH, etc., for Homebrew.' >> /Users/henryweller/.zprofile
+    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/henryweller/.zprofile
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+    brew install astro
+elif [[ $1 == 'windows' ]]
+then
+    winget install -e --id Astronomer.Astro   
 
-
-brew install astro
+elif [[ $1 == 'linux' ]]
+then
+    curl -sSL install.astronomer.io | sudo bash -s
+fi
 
 astro dev start
