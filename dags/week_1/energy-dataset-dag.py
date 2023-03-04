f44c53de4d0f26873a9b8a0b71ffd70a713eaf83
karamel <kenneth.odumah@gmail.com>
2023-03-04 11:06:23 +0400

    week 1 task

--- dags/week_1/energy-dataset-dag.py
+++ dags/week_1/energy-dataset-dag.py
@@ -2,7 +2,8 @@ from datetime import datetime
 from typing import List
 
 import pandas as pd
-from airflow.decorators import dag, task # DAG and task decorators for interfacing with the TaskFlow API
+from airflow.decorators import dag, task  # DAG and task decorators for interfacing with the TaskFlow API
+
 
 @dag(
     # This defines how often your DAG will run, or the schedule by which your DAG runs. In this case, this DAG
@@ -16,16 +17,15 @@ from airflow.decorators import dag, task # DAG and task decorators for interfaci
     # run will be for the next 30 mins, per the schedule_interval
     catchup=False,
     default_args={
-        "retries": 2, # If a task fails, it will retry 2 times.
+        "retries": 2,  # If a task fails, it will retry 2 times.
     },
-    tags=['example']) # If set, this tag is shown in the DAG view of the Airflow UI
+    tags=['example'])  # If set, this tag is shown in the DAG view of the Airflow UI
 def energy_dataset_dag():
     """
     ### Basic ETL Dag
     This is a simple ETL data pipeline example that demonstrates the use of
     the TaskFlow API using two simple tasks to extract data from a zipped folder
     and load it to GCS.
-
     """
 
     @task
@@ -34,35 +34,38 @@ def energy_dataset_dag():
         #### Extract task
         A simple task that loads each file in the zipped file into a dataframe,
         building a list of dataframes that is returned.
-
         """
         from zipfile import ZipFile
-        # TODO Unzip files into pandas dataframes
-
+        zip_path = "/usr/local/airflow/dags/data/energy-consumption-generation-prices-and-weather.zip"
+        with ZipFile(zip_path, mode="r") as z:
+            energy_data = [pd.read_csv(z.open(file)) for file in z.namelist()]
+        # first dataframe is generation, second is weather
+        return energy_data
 
     @task
     def load(unzip_result: List[pd.DataFrame]):
         """
-        #### Load task
-        A simple "load" task that takes in the result of the "transform" task, prints out the 
-        schema, and then writes the data into GCS as parquet files.
+        #### Load task A simple "load" task that takes in the result of the
+        "transform" task, prints out the schema, and then writes the data
+        into GCS as parquet files.
         """
-
         from airflow.providers.google.cloud.hooks.gcs import GCSHook
 
-        data_types = ['generation', 'weather']
+        data_types = ["generation", "weather"]
+        client = GCSHook()  # TODO Add GCS upload code
+        bucket = "corise-airflow-kod"
 
-        # GCSHook uses google_cloud_default connection by default, so we can easily create a GCS client using it
-        # https://github.com/apache/airflow/blob/207f65b542a8aa212f04a9d252762643cfd67a74/airflow/providers/google/cloud/hooks/gcs.py#L133
+        for i, df in enumerate(unzip_result):
+            # print dataframe schema
+            print(df.dtypes)
+            client.upload(
+                bucket_name=bucket,
+                object_name=f"week-1/{data_types[i]}.parquet",
+                data=df.to_parquet()
+            )
 
-        # The google cloud storage github repo has a helpful example for writing from pandas to GCS:
-        # https://github.com/googleapis/python-storage/blob/main/samples/snippets/storage_fileio_pandas.py
-        
-        client = GCSHook()     \
-        # TODO Add GCS upload code
-
-
-    # TODO Add task linking logic here
+    unzipped_result = extract()
+    load(unzipped_result)
 
 
 energy_dataset_dag = energy_dataset_dag()
